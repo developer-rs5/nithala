797d0a8bbe9a7c41754c4d384e4a47afb80f1906
Developer RS <164728448+developer-rs5@users.noreply.github.com>
2024-10-19 21:53:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,6 +58,10 @@ function takeCommand(message) {
     else if (message.includes("who are you")) {
         speak("I am a virtual assistant, created by Developer RS");
     }
+    else if ((message.includes("develop") || message.includes("developer") || message.includes("create") || message.includes("creator")) &&
+         (message.includes("you") || message.includes("your"))) {
+    speak("My developer is RS. and created me in html,css,js");
+    }    
     else if (message.includes("time")) {
         let time = new Date().toLocaleString(undefined, { hour: "numeric", minute: "numeric" });
         speak(time);
